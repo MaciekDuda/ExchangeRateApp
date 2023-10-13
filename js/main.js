e89340fe5cbbb41ed68ddfa90e021b32a91a77a2
MaciekDuda <maciejduda1626@gmail.com>
2023-10-13 15:08:36 +0200

    final version

---
+++ js/main.js
@@ -0,0 +1,32 @@
+const currencyOne = document.querySelector('#currency-one');
+const currencyTwo = document.querySelector('#currency-two');
+const amountOne = document.querySelector('.amount-one');
+const amountTwo = document.querySelector('.amount-two');
+const swapBtn = document.querySelector('.swap');
+const rateInfo = document.querySelector('.rate-info');
+
+const calculate = () => {
+	fetch(`https://open.er-api.com/v6/latest/${currencyOne.value}`)
+		.then((res) => res.json())
+		.then((data) => {
+			const currency1 = currencyOne.value;
+			const currency2 = currencyTwo.value;
+			const rate = data.rates[currency2];
+
+			rateInfo.textContent = `1 ${currency1} = ${rate.toFixed(3)} ${currency2}`;
+			amountTwo.value = (amountOne.value * rate).toFixed(2);
+		});
+};
+
+const swap = () => {
+	const oldValue = currencyOne.value;
+	currencyOne.value = currencyTwo.value;
+	currencyTwo.value = oldValue;
+  calculate();
+};
+
+currencyOne.addEventListener('change', calculate);
+currencyTwo.addEventListener('change', calculate);
+amountOne.addEventListener('input', calculate);
+swapBtn.addEventListener('click', swap);
+calculate();
